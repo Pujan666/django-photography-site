9680cbbed7fd83e44d55a1a4a8932198dcbc865b
Pujan666 <62142522+Pujan666@users.noreply.github.com>
2020-11-07 16:44:19 +0545

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -4,9 +4,9 @@ import api.views
 
 urlpatterns = [
 	url(r'^categories/(?P<categoryId>[0-9]+)$', api.views.CategoryImageList.as_view()),
-    	url(r'^categories/$', views.CategoriesList.as_view()),
-    	url(r'^feed/$', views.FeedActivityList.as_view()),
-    	url(r'^subscribe/$', views.Subscribe.as_view()),
+    	url(r'^categories/$', api.views.CategoriesList.as_view()),
+    	url(r'^feed/$', api.views.FeedActivityList.as_view()),
+    	url(r'^subscribe/$', api.views.Subscribe.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
